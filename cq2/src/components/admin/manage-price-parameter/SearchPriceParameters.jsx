7ad9abff03d8cf97b22b260b6b979c0cbdbd2aab
Jayakumar Jayaraman <india.jai@gmail.com>
2019-09-08 16:34:58 +0100

    updated

--- cq2/src/components/admin/manage-price-parameter/SearchPriceParameters.jsx
+++ cq2/src/components/admin/manage-price-parameter/SearchPriceParameters.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { DataTable } from 'primereact/datatable';
 
 export default class SearchPriceParameters extends Component {
 
