2a22e68fc5c97bac4c3eb6e6ca48046d939b798a
Jayakumar Jayaraman <india.jai@gmail.com>
2019-09-08 11:55:43 +0100

    state added

--- cq2/src/components/menu/Menu.js
+++ cq2/src/components/menu/Menu.js
@@ -4,100 +4,105 @@ import { PanelMenu } from 'primereact/panelmenu';
 
 export default class Menu extends Component {
 
+   constructor() {
+      super();
+      this.state = {
+         items: [
+            {
+               label: 'Create Custom Quote',
+               icon: 'pi pi-fw pl-plus'
+            },
+            {
+               label: 'Search Custom Quote',
+               icon: 'pi pi-fw pi-search'
+            },
+            {
+               label: 'Admin',
+               icon: 'pi pi-fw pi-cog',
+               items: [
+                  {
+                     label: 'Components',
+                     icon: 'pi pi-fw pi-th-large',
+                     items: [
+                        {
+                           label: 'Create',
+                           icon: 'pi pi-fw pi-plus'
+                        },
+                        {
+                           label: 'Search',
+                           icon: 'pi pi-fw pi-search'
+                        }
+                     ]
+                  },
+                  {
+                     label: 'Rules',
+                     icon: 'pi pi-fw pi-list',
+                     items: [
+                        {
+                           label: 'Create',
+                           icon: 'pi pi-fw pi-plus'
+                        },
+                        {
+                           label: 'Search',
+                           icon: 'pi pi-fw pi-search'
+                        }
+                     ]
+                  },
+                  {
+                     label: 'Price',
+                     icon: 'pi pi-fw pi-dollar',
+                     items: [
+                        {
+                           label: 'Create',
+                           icon: 'pi pi-fw pi-plus'
+                        },
+                        {
+                           label: 'Search',
+                           icon: 'pi pi-fw pi-search'
+                        }
+                     ]
+                  },
+                  {
+                     label: 'Price Parameters',
+                     icon: 'pi pi-fw pi-dollar',
+                     items: [
+                        {
+                           label: 'Create',
+                           icon: 'pi pi-fw pi-plus'
+                        },
+                        {
+                           label: 'Search',
+                           icon: 'pi pi-fw pi-search'
+                        }
+                     ]
+                  },
+                  {
+                     label: 'Users',
+                     icon: 'pi pi-fw pi-user',
+                     items: [
+                        {
+                           label: 'Create',
+                           icon: 'pi pi-fw pi-user-plus'
+                        },
+                        {
+                           label: 'Search',
+                           icon: 'pi pi-fw pi-search'
+                        }
+                     ]
+                  }
+               ]
+            }
+         ]
+      }
+   }
+
    render() {
 
-      const items = [
-         {
-            label: 'Create Custom Quote',
-            icon: 'pi pi-fw pl-plus'
-         },
-         {
-            label: 'Search Custom Quote',
-            icon: 'pi pi-fw pi-search'
-         },
-         {
-            label: 'Admin',
-            icon: 'pi pi-fw pi-cog',
-            items: [
-               {
-                  label: 'Components',
-                  icon: 'pi pi-fw pi-th-large',
-                  items: [
-                     {
-                        label: 'Create',
-                        icon: 'pi pi-fw pi-plus'
-                     },
-                     {
-                        label: 'Search',
-                        icon: 'pi pi-fw pi-search'
-                     }
-                  ]
-               },
-               {
-                  label: 'Rules',
-                  icon: 'pi pi-fw pi-list',
-                  items: [
-                     {
-                        label: 'Create',
-                        icon: 'pi pi-fw pi-plus'
-                     },
-                     {
-                        label: 'Search',
-                        icon: 'pi pi-fw pi-search'
-                     }
-                  ]
-               },
-               {
-                  label: 'Price',
-                  icon: 'pi pi-fw pi-dollar',
-                  items: [
-                     {
-                        label: 'Create',
-                        icon: 'pi pi-fw pi-plus'
-                     },
-                     {
-                        label: 'Search',
-                        icon: 'pi pi-fw pi-search'
-                     }
-                  ]
-               },
-               {
-                  label: 'Price Parameters',
-                  icon: 'pi pi-fw pi-dollar',
-                  items: [
-                     {
-                        label: 'Create',
-                        icon: 'pi pi-fw pi-plus'
-                     },
-                     {
-                        label: 'Search',
-                        icon: 'pi pi-fw pi-search'
-                     }
-                  ]
-               },
-               {
-                  label: 'Users',
-                  icon: 'pi pi-fw pi-user',
-                  items: [
-                     {
-                        label: 'Create',
-                        icon: 'pi pi-fw pi-user-plus'
-                     },
-                     {
-                        label: 'Search',
-                        icon: 'pi pi-fw pi-search'
-                     }
-                  ]
-               }
-            ]
-         }
-         
-      ];
 
 
       return (
          <div id="leftMenu">
-            <PanelMenu model={items} style={{fontSize:'12px'}}/>
+            <PanelMenu model={this.state.items} style={{ fontSize: '12px' }} />
          </div>
       )
    }
