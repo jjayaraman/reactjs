c03961455d9c14d6a29b7ab1d5850ca277ea35f2
jjayaraman <india.jai@gmail.com>
2019-09-17 06:31:33 +0100

    validation

--- crud/src/components/CreateEmployee.jsx
+++ crud/src/components/CreateEmployee.jsx
@@ -3,6 +3,7 @@ import { Alert, Form, Col, Row, Button, Card } from 'react-bootstrap';
 import EmployeeForm from './EmployeeForm';
 import EmployeeService from '../services/EmployeeService';
 
+import * as  Yup from 'yup'
 
 export default class CreateEmployee extends React.Component {
 
@@ -16,10 +17,31 @@ export default class CreateEmployee extends React.Component {
                 'birthDate': '',
                 'hireDate': '',
                 'gender': ''
+            },
+            touched: {
+                'firstName': false,
+                'lastName': false,
+                'birthDate': false,
+                'hireDate': false,
+                'gender': false
             }
         }
     }
 
+    validationSchema =
+        Yup.object().shape(
+            {
+                'firstName': Yup.string()
+                    .required('First name is required'),
+                //                    .max(10, 'Max 10 characters allowed'),
+                'lastName': Yup.string()
+                    .required('Last name is required'),
+                //                    .min(5, 'At least 5 characters'),
+                'birthDate': Yup.date().required('Birth date is required'),
+                'hireDate': Yup.date().required('Hire Date is required'),
+                'gender': Yup.string().required('Gender is required'),
+            }
+        )
     save = () => {
         console.log("saving :: ", this.state.employee);
         this.employeeService.createEmployee(this.state.employee)
@@ -33,9 +55,34 @@ export default class CreateEmployee extends React.Component {
             })
     }
 
-    handleChange = e => {
-        this.setState({ employee: { ...this.state.employee, [e.target.id]: e.target.value } });
+    handleOnChange = e => {
+        this.setState(
+            {
+                employee: { ...this.state.employee, [e.target.id]: e.target.value },
+                touched: { ...this.state.touched, [e.target.id]: e.target.value ? true : false }
+            });
     }
+
+    // handleOnBlur = e => {
+    //     this.setState(
+    //         {
+    //             errors: { ...this.state.errors, [e.target.id]: e.target.value }
+    //         });
+    // }
+
+    handleOnBlur = () => {
+        console.log('handle on blur ');
+
+        this.validationSchema.validate(this.state.employee)
+            .then(result => {
+                console.log('validation result ', result);
+                this.setState({ errors: { ...this.state.errors, errors: result } })
+            }).catch(err => {
+                console.log('validation error :: ', err.value);
+                this.setState({ errors: err })
+            });
+    }
+
 
     render() {
 
@@ -44,11 +91,12 @@ export default class CreateEmployee extends React.Component {
                 {/* <Form> */}
                 <Alert variant='primary'>Employee</Alert>
 
-                <Card style={{ width: '500px' }}>
+                <Card>
                     <Card.Header>Create Employee</Card.Header>
                     <Card.Body>
 
-                        <EmployeeForm employee={this.state.employee} handleChange={this.handleChange} ></EmployeeForm>
+                        <EmployeeForm employee={this.state.employee} handleOnChange={this.handleOnChange}
+                            handleOnBlur={this.handleOnBlur} touched={this.state.touched} errors={this.state.errors}></EmployeeForm>
                         <Row>
                             <Col>
                                 <Button variant='primary' type='button' onClick={this.save}>Create</Button>
--- crud/src/components/EmployeeForm.jsx
+++ crud/src/components/EmployeeForm.jsx
@@ -2,95 +2,65 @@ import React, { Component } from 'react'
 import { Form } from 'react-bootstrap'
 
 
-import { Formik, ErrorMessage } from 'formik'
-import * as  Yup from 'yup'
-
 export default class EmployeeForm extends Component {
 
     constructor(props) {
         super(props);
     }
 
-    /** Generic handle change events for all fields */
-    handleChange = e => {
-        this.props.handleChange(e);
+    initalValues = {
+        'firstName': '',
+        'lastName': '',
+        'birthDate': '',
+        'hireDate': '',
+        'gender': ''
     }
+
+
 
     render() {
 
-        const { employee } = this.props;
+        const { employee, handleOnChange, handleOnBlur, touched, errors } = this.props;
 
-        const initalValues = {
-            'firstName': '',
-            'lastName': '',
-            'birthDate': '',
-            'hireDate': '',
-            'gender': ''
-        }
-        const validationSchema =
-            Yup.object().shape(
-                {
-                    'firstName': Yup.string()
-                        .required('First name is required')
-                        .length(10)
-                        .max(10, 'Max 10 characters allowed'),
-                    'lastName': Yup.string()
-                        .required('Last name is required')
-                        .min(5, 'At least 5 characters'),
-                    'birthDate': Yup.date().required('Birth date is required'),
-                    'hireDate': Yup.date().required('Hire Date is required'),
-                    'gender': Yup.string().required('Gender is required'),
-                }
-            )
-
-        const render = ({ errors, status, touched }) =>
-            (
+        return (
+            <div>
+                <pre style={{ color: "brown", border: "1px solid blue", padding: '5px' }}>
+                    <b>Debuger</b> <br></br>
+                    {JSON.stringify(this.props, null, 3)}
+                </pre>
+                <br></br>
                 <form>
                     <Form.Group controlId="firstName">
                         <Form.Label>First name</Form.Label>
-                        <Form.Control type="text" value={employee.firstName} onChange={this.handleChange} placeholder="Enter first name" />
-                        <ErrorMessage name="firstName" component="div" className="invalid-feedback" />
+                        <Form.Control type="text" value={employee.firstName} onChange={handleOnChange} onBlur={handleOnBlur} placeholder="Enter first name" />
+
                     </Form.Group>
 
                     <Form.Group controlId="lastName">
                         <Form.Label>Last name</Form.Label>
-                        <Form.Control type="text" value={employee.lastName} onChange={this.handleChange} placeholder="Enter last name" />
+                        <Form.Control type="text" value={employee.lastName} onChange={handleOnChange} placeholder="Enter last name" />
                     </Form.Group>
 
                     <Form.Group controlId="birthDate">
                         <Form.Label>Date of birth</Form.Label>
-                        <Form.Control type="date" value={employee.birthDate} onChange={this.handleChange} />
+                        <Form.Control type="date" value={employee.birthDate} onChange={handleOnChange} onBlur={handleOnBlur} />
                     </Form.Group>
 
                     <Form.Group controlId="hireDate">
                         <Form.Label>Date of hire</Form.Label>
-                        <Form.Control type="date" value={employee.hireDate} onChange={this.handleChange} />
+                        <Form.Control type="date" value={employee.hireDate} onChange={handleOnChange} />
                     </Form.Group>
 
                     <Form.Group controlId="gender">
                         <Form.Label>Gender</Form.Label>
-                        <Form.Control as="select" value={employee.gender} onChange={this.handleChange}>
+                        <Form.Control as="select" value={employee.gender} onChange={handleOnChange}>
                             <option value="">Please select</option>
                             <option value="F">Female</option>
                             <option value="M">Male</option>
                         </Form.Control>
                     </Form.Group>
                 </form >
-            )
-
-        // onSubmit = (fields) => {
-        //     console.log('onSubmit : ', fields);
-
-        // }
-
-        return (
-            <Formik
-                initalValues={initalValues}
-                validationSchema={validationSchema}
-                onSubmit={fields => {
-                    alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4))
-                }}
-                render={render} />
+            </div>
         )
     }
 }
