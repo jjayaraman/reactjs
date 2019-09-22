c23f3f85bbde3ec7339c1c85306617a8f00ba2a6
Jayakumar Jayaraman <india.jai@gmail.com>
2019-09-22 18:47:23 +0100

    with redux

---
+++ redux/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      With Redux
+    </div>
+  );
+}
+
+export default App;
---
+++ redux/src/actions/TodoActionTypes.js
@@ -0,0 +1,7 @@
+/** Todo Action types */
+
+export const ADD_TODO = 'AddTodoAction';
+
+export const EDIT_TODO = 'EditTodoAction';
+
+export const DELETE_TODO = 'DeleteTodoAction';
