f430baa46e80c561dac05e114d0b76d72de6241b
jjayaraman <india.jai@gmail.com>
2020-02-22 09:21:09 +0000

    tests

--- react-jest/src/container/User.jsx
+++ react-jest/src/container/User.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { UserList } from '../component/UserList'
 
 export default class User extends Component {
 
@@ -14,7 +15,7 @@ export default class User extends Component {
     render() {
         return (
             <div>
-
+                <UserList />
             </div>
         )
     }
--- react-jest/src/setupTests.js
+++ react-jest/src/setupTests.js
@@ -6,7 +6,14 @@ import '@testing-library/jest-dom/extend-expect';
 
 
 // For Testing with Enzyme
+import React from 'react'
 import Adapter from 'enzyme-adapter-react-16'
-import Enzyme from 'enzyme'
+import Enzyme, { shallow, render, mount } from 'enzyme'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+// Global imports for all test files
+global.React = React
+global.shallow = shallow
+global.mount = mount
+global.render = render
